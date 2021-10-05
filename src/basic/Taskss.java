f38b03d560bb874874ae4e430e56dd3ca4cfc1fe
Serhii Ivanov <sivanov047@gmail.com>
2021-10-05 16:30:37 +0300

    Six and seven are done.

--- src/basic/Taskss.java
+++ src/basic/Taskss.java
@@ -1,5 +1,6 @@
 package basic;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Taskss {
@@ -14,7 +15,7 @@ public class Taskss {
 class Arguments {
     public static void main(String[] args) {
         for (int i = args.length - 1; i >= 0; i--)
-            System.out.println("Reverse arguments " + args[i]); // Why it doesnt work?
+            System.out.println("Reverse arguments " + args[i]); // Writing the arguments in reverse.
     }
 }
 
@@ -55,7 +56,50 @@ class InputOutputNumbers {
             sum = sum + numbers % 10;
             numbers = numbers / 10;
         }
-        System.out.println("Your quanity of numbers is " + sum);
+        System.out.println("Your quanity of numbers is " + sum); // Sum of numbers from console.
     }
 }
 
+class EvenOddNumbers {
+    public static void main(String[] args) {
+        System.out.println("Input your numbers");
+        Scanner scanner = new Scanner(System.in);
+        String num = scanner.nextLine();
+        String[] numbers = num.split(" ");
+        ArrayList<Integer> even = new ArrayList<>();
+        ArrayList<Integer> odd = new ArrayList<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            int number = Integer.parseInt(numbers[i]);
+            if (number % 2 == 0)
+                even.add(number);
+            else
+                odd.add(number);
+        }
+        System.out.println("Even numbers : " + even);
+        System.out.println("Odd numbers : " + odd); //Writing even and odd numbers
+    }
+}
+
+class RankNumber {
+    public static void main(String[] args) {
+        System.out.println("Input your numbers");
+        Scanner scanner = new Scanner(System.in);
+        String num = scanner.nextLine();
+        String[] numbers = num.split(" ");
+        int min = 0;
+        int max = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            int current = Integer.parseInt(numbers[i]);
+            if (min == 0) {
+                min = current;
+                max = current;
+            }
+            if (min > current) min = current;
+            if (max < current) max = current;
+        }
+        System.out.println("Min number is "+min);
+        System.out.println("Max number is "+max); // Choosing the biggest and the smallest number.
+    }
+}
