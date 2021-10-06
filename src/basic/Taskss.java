03ee9e694ba0bd6a2f5dcd184890e7ae0cfa7f7e
Serhii Ivanov <sivanov047@gmail.com>
2021-10-07 00:28:15 +0300

    Homework is done.

--- src/basic/Taskss.java
+++ src/basic/Taskss.java
@@ -92,14 +92,81 @@ class RankNumber {
 
         for (int i = 0; i < numbers.length; i++) {
             int current = Integer.parseInt(numbers[i]);
-            if (min == 0) {
-                min = current;
-                max = current;
-            }
+            if (current == 0) min = 0, max = 0;
+
             if (min > current) min = current;
             if (max < current) max = current;
         }
-        System.out.println("Min number is "+min);
-        System.out.println("Max number is "+max); // Choosing the biggest and the smallest number.
+        System.out.println("Min number is " + min);
+        System.out.println("Max number is " + max); // Choosing the biggest and the smallest number.
+    }
+}
+
+class NumbersDivides {
+    public static void main(String[] args) {
+        System.out.println("Input your numbers");
+        Scanner scanner = new Scanner(System.in);
+        String num = scanner.nextLine();
+        String[] numbers = num.split(" ");
+        ArrayList<Integer> division = new ArrayList<>();
+        ArrayList<Integer> division2 = new ArrayList<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            int number = Integer.parseInt(numbers[i]);
+            if ((number % 3 == 0) || (number % 9 == 0)) division.add(number);
+            if ((number % 5 == 0) && (number % 7 == 0)) division2.add(number);
+
+        }
+        System.out.println("Numbers which divides on 3 or 9 : " + division);
+        System.out.println("Numbers which divides on 5 and 7 : " + division2);
+    }
+}
+
+class NumbersResult {
+    public static void main(String[] args) {
+        System.out.println("Input your numbers");
+        Scanner scanner = new Scanner(System.in);
+        String num = scanner.nextLine();
+        String[] numbers = num.split(" ");
+        ArrayList<Integer> result3 = new ArrayList<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            int number = Integer.parseInt(numbers[i]);
+
+            int hundreds = number / 100;
+            int decades = (number % 100) / 10;
+            int ones = number % 10;
+
+            if (hundreds != decades && hundreds != ones && decades != ones) result3.add(number);
+        }
+        System.out.println(result3); /// All digits in numbers are different
+    }
+}
+
+class HappyNumbers {
+    public static void main(String[] args) {
+        System.out.println("Input your numbers");
+        Scanner scanner = new Scanner(System.in);
+        String num = scanner.nextLine();
+        String[] numbers = num.split(" ");
+        ArrayList<Integer> happyNumbers = new ArrayList<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            int number = Integer.parseInt(numbers[i]);
+
+            int a = number / 100_000;
+            int b = (number % 100_000) /10_000;
+            int c = (number % 10_000) / 1000;
+            int d = (number % 1000) / 100;
+            int e = (number % 100) / 10;
+            int f = number % 10;
+
+            int sum123 = a + b + c;
+            int sum456 = d + e + f;
+
+            if (sum123 == sum456) happyNumbers.add(number);
+
+        }
+        System.out.println("happy numbers : " + happyNumbers); //
     }
 }
