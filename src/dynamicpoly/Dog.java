26dbc547f4fd2b757fa53398357c2cd1b0e2994d
Serhii Ivanov <sivanov047@gmail.com>
2021-11-01 18:05:40 +0200

    Add completed tasks from the 1st task page

---
+++ src/dynamicpoly/Dog.java
@@ -0,0 +1,9 @@
+package dynamicpoly;
+
+public class Dog implements Voicable{
+    @Override
+    public void voice() {
+        System.out.println("If you thing, that you can leave this room without your ass get bitten, you're dead wrong!");
+        System.out.println("Bark! bark!");
+    }
+}
---
+++ src/dynamicpoly/Voicable.java
@@ -0,0 +1,5 @@
+package dynamicpoly;
+
+public interface Voicable {
+    void voice();
+}
